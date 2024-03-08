dd35a797891486cd1788ed6d70dcbafafa5588b8
Aliaksei Kalosha <aliaksei.kalosha@outlook.com>
2024-03-09 00:05:49 +0100

    Add light and DeathFloor

---
+++ PlatformerSkeletonProject/Assets/Scripts/DeathFloor.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathFloor : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform deathFloor;
+    [SerializeField] private float minHeightToActivate;
+    [SerializeField] private float speed = 1.0f;
+    private bool hasPasMinHeight;
+
+    private void Update()
+    {
+        if(minHeightToActivate < player.position.y)
+        {
+            hasPasMinHeight = true;
+        }
+        if (hasPasMinHeight)
+        {
+            deathFloor.position += Vector3.up * speed * Time.deltaTime;
+
+            if (deathFloor.position.y > player.position.y)
+            {
+                Debug.Log("To do death screen");
+                SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+}
